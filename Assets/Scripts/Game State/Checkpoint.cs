dfddaee5ac25700e1bd23ecd13619efa4c2801d1
Denya56 <56lkj546@gmail.com>
2023-01-23 16:40:17 +0100

    Greybox merge adjustments
    Adjustments in dev merge (animation, puzzles, checkpoint) for Greybox preparation

---
+++ Assets/Scripts/Game State/Checkpoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public SpriteRenderer _sr { get; private set; }
+    void Start()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _sr.color = Color.green;
+            GameEventSystem.Instance.SaveData();
+        }
+    }
+}
--- Assets/Scripts/Traps/Spike.cs
+++ Assets/Scripts/Traps/Spike.cs
@@ -8,7 +8,7 @@ public class Spike : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Destroy(collision.gameObject);
+            GameEventSystem.Instance.LoadData();
         }
     }
 }
