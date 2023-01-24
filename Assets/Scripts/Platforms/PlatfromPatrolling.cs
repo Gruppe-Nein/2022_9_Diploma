56c7ffd4cca85f3d0be55e45bb2061b9220fa6a6
K1rovets <nurzhigitovkir@gmail.com>
2023-01-24 19:20:56 +0100

    Greybox small fixes
    Made small changes to the greybox:
    1) deleted cannon that trashed hierarchy
    2) changed moving platforms end points' positions
    3) changed layer for cannonball (from "ground" to "default")
    4) Some code changes for patrolling platform script code

--- Assets/Scripts/Platforms/PlatfromPatrolling.cs
+++ Assets/Scripts/Platforms/PlatfromPatrolling.cs
@@ -44,7 +44,6 @@ public class PlatfromPatrolling : MonoBehaviour
 
     private void FixedUpdate()
     {
-        CheckOverlapCircle();
         _rBody2D.velocity = _moveDirection * _platformSpeed;
     }
 
