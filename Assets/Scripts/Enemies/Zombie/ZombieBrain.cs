0f46fea8c11a9adebf8ed38d03a208f51f8841a8
Denya56 <56lkj546@gmail.com>
2023-03-12 14:34:42 +0100

    Adjusted enemy death functionality
    Now objects having "Fallable" script kill enemy instead enemies dying after an object falls on them. Same with OutOfMap object

--- Assets/Scripts/Enemies/Zombie/ZombieBrain.cs
+++ Assets/Scripts/Enemies/Zombie/ZombieBrain.cs
@@ -25,12 +25,4 @@ public class ZombieBrain : EnemyBrain
     {
         
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Rock") || collision.gameObject.name == "TouchDie")
-        {
-            Destroy(gameObject);
-        }
-    }
 }
