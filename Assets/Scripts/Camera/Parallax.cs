24611c991881534537ac2a0896e5a8c7251ee526
K1rovets <nurzhigitovkir@gmail.com>
2023-11-16 13:01:15 +0100

    Parallax configuration
    *Forgot to configure parallax on cameras

--- Assets/Scripts/Camera/Parallax.cs
+++ Assets/Scripts/Camera/Parallax.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startpos;
-    public float parallaxFactor;
+    [SerializeField] private float _parallaxFactor;
+    [SerializeField] private float _pixelsPerUnit;
+    private float _startpos;
+
     public GameObject cam;
 
     void Start()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        _startpos = transform.position.x;
     }
 
     void Update()
     {
-        float distance = cam.transform.position.x * parallaxFactor;
-        Vector3 newPosition = new Vector3(startpos + distance, transform.position.y, transform.position.z);
-        transform.position = newPosition;
+        float distance = cam.transform.position.x * _parallaxFactor;
+        Vector3 newPosition = new Vector3(_startpos + distance, transform.position.y, transform.position.z);
+        transform.position = PixelPerfectClamp(newPosition, _pixelsPerUnit);
+    }
+
+    private Vector3 PixelPerfectClamp(Vector3 locationVector, float pixelsPerUnit)
+    {
+        Vector3 vectorInPixels = new Vector3(Mathf.CeilToInt(locationVector.x * pixelsPerUnit), Mathf.CeilToInt(locationVector.y * pixelsPerUnit), Mathf.CeilToInt(locationVector.z * pixelsPerUnit));
+        return vectorInPixels / pixelsPerUnit;
     }
 }
