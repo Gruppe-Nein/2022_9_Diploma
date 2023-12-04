edcf425e987a7248f04cfa08d311f645da8f49ef
K1rovets <nurzhigitovkir@gmail.com>
2023-12-04 18:45:57 +0100

    Light source reaction
    *Added time zone behavior for checkpoints, torches, candelabers and ropetorches lights

--- Assets/Scripts/Props/FlickeringLight.cs
+++ Assets/Scripts/Props/FlickeringLight.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
@@ -7,22 +6,45 @@ using UnityEngine.Rendering.Universal;
 public class FlickeringLight : MonoBehaviour
 {
     private Light2D _light;
+    private bool _isStopped;
     [SerializeField] private float _minIntensity;
     [SerializeField] private float _maxIntensity;
 
 
     private void Start()
     {
+        _isStopped = false;
         _light = GetComponent<Light2D>();
         StartCoroutine(Flicker());
     }
 
     IEnumerator Flicker()
     {
-        while (true)
+        while (!_isStopped)
         {
             _light.intensity = Random.Range(_minIntensity, _maxIntensity);
             yield return new WaitForSeconds(0.1f);
         }
     }
+
+    #region PLATFORM TIME ZONE BEHAVIOR
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ChronoZone"))
+        {
+            Debug.Log("FLICKER STOP");
+            _isStopped = true;
+            StopCoroutine(Flicker());
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("ChronoZone"))
+        {
+            Debug.Log("FLICKER GO");            
+            _isStopped = false;
+            StartCoroutine(Flicker());
+        }
+    }
+    #endregion
 }
